5f50876850b445dd68fc6eeb8cba5df298a7dfab
Artem Hoshkoderia <goshkoderia@gmail.com>
2022-02-08 14:03:45 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+import {throttle} from 'lodash';
+
+const onPlay = function(data){
+      const stringifyData = JSON.stringify(data);
+      localStorage.setItem('video-player-current-time',stringifyData)
+}
+player.on('timeupdate', throttle(onPlay,1000));
+
+function resumePlayBack(){
+    if (JSON.parse(localStorage.getItem('video-player-current-time')) === null){
+        return;
+    }
+    const paused = JSON.parse(localStorage.getItem('video-player-current-time'))['seconds'];
+    player.setCurrentTime(paused).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+}
+resumePlayBack();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,15 @@
 
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+    button: document.querySelector('button'),
+};
+
+refs.form.addEventListener('input',onFormInput);
+
+function onFormInput (event){
+   const message = event.currentTarget.value;
+   localStorage.setItem('feedback-form-state',message);
+}
